54913a07a92779d60b967265752cee4286fd3d93
gabriela <gykimura10@gmail.com>
2019-10-10 16:50:54 -0300

    Add exercicio

--- python/whileforif/5-mult35.py
+++ python/whileforif/5-mult35.py
@@ -1,8 +1,11 @@
 
 def mult35(limit):
-
-
+	summ = 0
+	for i in range(limit+1):
+		if((i%3==0)or(i%5==0)):
+			summ += i
+	return summ
 
 while(1):
 	num = int(input("Insira um numero: "))
-	mult35
+	print("Soma dos multiplos de 3 e 5: ", mult35(num))
