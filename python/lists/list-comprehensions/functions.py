029281d6304fc1f77ad5ec5d5d8439615eee80ab
gabriela <gykimura10@gmail.com>
2019-10-11 16:12:57 -0300

    add exercicios

--- python/lists/list-comprehensions/functions.py
+++ python/lists/list-comprehensions/functions.py
@@ -4,7 +4,12 @@ def zip(list1, list2):
 	print ([(list1[i], list2[i]) for i in range(len(list1))])
 
 
-def map(list1,fuction):
-	return ([function(list1[i]) for i in range(len(list1))])
+def map(list1,square):
+	print ([square(list1[i]) for i in range(len(list1))])
 	
-	
+
+def filter(list1, even):
+	print ([list1[i] for i in range(len(list1)) if even(list1[i]) == True ])
+
+
+
--- python/lists/list-comprehensions/main.py
+++ python/lists/list-comprehensions/main.py
@@ -1,16 +1,15 @@
 import functions
 
-
 def square(x):
 	return (x*x)
 
-
+def even(x):
+	return (x%2==0)
 
 list1 = [1,2,3]
 list2 = ["a","b","c"]
 
-functions.zip(list1,list2)
-
-functions.map(list1, square)
-
+#functions.zip(list1,list2)
+#functions.map(list1, square)
+functions.filter(list1, even)
 
