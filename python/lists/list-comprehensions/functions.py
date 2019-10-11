cbab28014894578cf63bb59975d6f3eb9d284db8
gabriela <gykimura10@gmail.com>
2019-10-11 17:06:46 -0300

    Add exercicios

--- python/lists/list-comprehensions/functions.py
+++ python/lists/list-comprehensions/functions.py
@@ -1,5 +1,4 @@
 
-
 def zip(list1, list2):
 	print ([(list1[i], list2[i]) for i in range(len(list1))])
 
@@ -12,4 +11,24 @@ def filter(list1, even):
 	print ([list1[i] for i in range(len(list1)) if even(list1[i]) == True ])
 
 
+def triplets(n):
+	print([(a,c-a,c) for c in range(2,n) for a in range(1,c//2+1)])
 
+def enumerate(list1):
+	print([(i,list1[i]) for i in range(len(list1))])
+
+def two_dimensional_array(d1, d2):
+	print([[None]*d2 for i in range(d1)]) 
+
+def parse_csv(file_name):
+	f = open(file_name)
+	lines = f.readlines()
+	print ([[line[:-1].split(',')] for line in lines])
+
+def generalized_parse_csv(file_name, d, c):
+	f = open(file_name)
+	lines = f.readlines()
+	print ([[line[:-1].split(d)] for line in lines if line[0]!=c])
+
+def mutate(word):
+	
--- python/lists/list-comprehensions/main.py
+++ python/lists/list-comprehensions/main.py
@@ -8,8 +8,19 @@ def even(x):
 
 list1 = [1,2,3]
 list2 = ["a","b","c"]
-
-#functions.zip(list1,list2)
-#functions.map(list1, square)
+"""
+functions.zip(list1,list2)
+functions.map(list1, square)
 functions.filter(list1, even)
+functions.triplets(5)
+functions.enumerate(list2)
 
+d1 = int(input("Insira dimensao 1: "))
+d2 = int(input("Insira dimensao 2: "))
+functions.two_dimensional_array(d1,d2)
+"""
+
+file_name = input("Insira nome de arquivo: ")
+#functions.parse_csv(file_name)
+d = input("Insira simbolo de separacao: ")
+functions.generalized_parse_csv(file_name,d,'#')
